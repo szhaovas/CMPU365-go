c950324d76be8f45915b37debf654b0794dbab19
Zhao Shihan <szhao@vassar.edu>
2019-05-13 23:14:29 -0400

    2019-05-13.23:14:28

--- data.lisp
+++ data.lisp
@@ -1,6 +1,6 @@
 (defun list-gen
   (step)
-  (let ((counter 0.0000001)
+  (let ((counter 0)
         (result nil))
     (while (<= counter 1)
       (push counter result)
@@ -10,18 +10,17 @@
 (defun k-test
   (match-number step)
   (let
-    ((k-values (list-gen step))
-     (win-rates nil))
+    ((k-values (list-gen step)))
     (while k-values
       (let
         ((k (pop k-values))
          (wins 0))
+        (when (zerop k) (setf k 0.0000001))
         (dotimes (i match-number)
                  (when (compete-mcrave-benchmark 1000 k)
                    (incf wins)))
-        (push (/ wins match-number) win-rates)))
-    (with-open-file (str "k-test.txt"
-                         :direction :output
-                         :if-exists :supersede
-                         :if-does-not-exist :create)
-      (format str "~A" (reverse win-rates)))))
+        (with-open-file (str "k-test.txt"
+                             :direction :output
+                             :if-exists :append
+                             :if-does-not-exist :create)
+          (format str "~A,~A~%" k (/ wins match-number)))))))
